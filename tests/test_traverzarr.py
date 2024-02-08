8f9870a0d963287d8fa68e6b1c475f77d84fe16f
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2024-02-08 13:12:44 -0500

    MVP is up

--- tests/test_traverzarr.py
+++ tests/test_traverzarr.py
@@ -4,21 +4,41 @@ import sys
 from pathlib import Path
 from traverzarr.schema import (
     ZarrLink,
-    ZarrJSON,
+    ZarrGroupJSON,
+    ZarrArrayJSON,
+    read_zarr_group_json, 
 )
 
-TEST_DATA = Path(__file__).parent / 'test_data'
+TEST_DATA = Path(__file__).parent / "test_data"
 
 
 @pytest.fixture
 def zarr_json() -> dict:
-    return json.loads((TEST_DATA / 'zarr.json').read_text())
+    return json.loads((TEST_DATA / "zarr.json").read_text())
 
 
-def test_schema(zarr_json: dict) -> None:
-    zarr = ZarrJSON(**zarr_json)
-    assert zarr.attributes == zarr_json['attributes']
-    assert zarr.zarr_format == zarr_json['zarr_format']
-    assert zarr.node_type == zarr_json['node_type']
-    assert zarr.links == [ZarrLink(**link) for link in zarr_json['links']]
+def test_group_schema(zarr_json: dict) -> None:
+    zarr = read_zarr_group_json(TEST_DATA / "zarr.json")
+    assert zarr.attributes == zarr_json["attributes"]
+    assert zarr.zarr_format == zarr_json["zarr_format"]
+    assert zarr.node_type == zarr_json["node_type"]
+    assert zarr.links == [ZarrLink(**link) for link in zarr_json["links"]]
+    
+    assert isinstance(zarr.href, Path)
+    assert bool(zarr.href)
+    
+    # the root and parent aren't in our test dir
+    with pytest.raises(ValueError):
+        zarr.root
+
+    with pytest.raises(ValueError):
+        zarr.parent
+
+    groups_list = list(zarr.groups)
+    assert len(groups_list) == 1
+    assert isinstance(groups_list[0], ZarrGroupJSON)
+
+    arrays_list = list(zarr.arrays)
+    assert len(arrays_list) == 1
+    assert isinstance(arrays_list[0], ZarrArrayJSON)
 
--- traverzarr/schema.py
+++ traverzarr/schema.py
@@ -1,32 +1,92 @@
-from pydantic import BaseModel, Field
-from attr import frozen
-from typing import Literal
+import json
+from pydantic import BaseModel
+from typing import Literal, Iterable
 from enum import IntEnum
+from pathlib import Path
+
 
 class ZarrVersions(IntEnum):
     v1 = 1
     v2 = 2
     v3 = 3
 
+
 class ZarrLink(BaseModel):
     href: str
-    rel: Literal['root', 'parent', 'self', 'array', 'group']
+    rel: Literal["root", "parent", "self", "array", "group"]
 
-class ZarrJSON(BaseModel):
+class ZarrGroupJSON(BaseModel):
     """Used to verify that the input is a valid Zarr JSON"""
+
     attributes: dict
     zarr_format: ZarrVersions
-
-    # what are the different node_types
+    parent_dir: Path
     node_type: str
     links: list[ZarrLink]
 
+    @property
+    def groups(self) -> Iterable["ZarrGroupJSON"]:
+        for group in filter(lambda x: x.rel == "group", self.links):
+            link_href = self.parent_dir / Path(group.href)
+            if not link_href.exists():
+                raise ValueError(f"Group link does not exist: {link_href}")
+            yield read_zarr_group_json(link_href)
 
-class ZarrTraverser:
+    @property
+    def arrays(self) -> Iterable["ZarrArrayJSON"]:
+        if self.node_type == "array":
+            raise ValueError("Zarr arrays cannot contain sub-arrays!")
+        for array in filter(lambda x: x.rel == "array", self.links):
+            link_href = self.parent_dir / Path(array.href)
+            if not link_href.exists():
+                raise ValueError(f"Array link does not exist: {link_href}")
+            yield read_zarr_array_json(link_href)
 
-    def __init__(self, zarr_json: ZarrJSON):
-        if not isinstance(zarr_json, ZarrJSON):
-            raise ValueError('zarr_json must be a ZarrJSON object')
-        self.attributes = zarr_json.attributes
+    @property
+    def href(self) -> Path:
+        for link in self.links:
+            if link.rel == "self":
+                return self.parent_dir / Path(link.href)
+        raise ValueError("No self link found")
 
+    @property
+    def root(self) -> "ZarrGroupJSON":
+        for link in self.links:
+            if link.rel == "root":
+                link_href = self.parent_dir / Path(link.href)
+                if not link_href.exists():
+                    raise ValueError(f"Root link does not exist: {link_href}")
+                return read_zarr_group_json(link_href)
+        raise ValueError("No root link found")
+
+    @property
+    def parent(self) -> "ZarrGroupJSON":
+        for link in self.links:
+            if link.rel == "parent":
+                link_href = self.parent_dir / Path(link.href)
+                if not link_href.exists():
+                    raise ValueError(f"Parent link does not exist: {link_href}")
+                return read_zarr_group_json(link_href)
+        raise ValueError("No parent link found")
+
+
+class ZarrArrayJSON(ZarrGroupJSON):
+    shape: list[int]
+    data_type: str
+    chunk_grid: dict
+    chunk_key_encoding: dict
+    fill_value: int | float | str | None
+    codecs: list[dict]
+    attributes: dict
+
+
+def read_zarr_group_json(file_path: Path) -> ZarrGroupJSON:
+    parent_dir = file_path.parent
+    zarr_json = json.loads(file_path.read_text())
+    return ZarrGroupJSON(**zarr_json, parent_dir=parent_dir)
+
+def read_zarr_array_json(file_path: Path) -> ZarrArrayJSON:
+    parent_dir = file_path.parent
+    zarr_json = json.loads(file_path.read_text())
+    return ZarrArrayJSON(**zarr_json, parent_dir=parent_dir)
 
